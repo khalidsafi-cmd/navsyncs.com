1d0e6e8e9aae1cb8412092cb97946be618201608
Ahmad Khalid Safi <khalidsafi031@gmail.com>
2026-01-14 13:36:38 +0430

    Remove testimonial carousel initialization
    Deleted the Owl Carousel setup for the testimonial section from main.js. This may be in preparation for refactoring, removing unused features, or switching to a different carousel implementation.

--- js/main.js
+++ js/main.js
@@ -58,35 +58,6 @@
     });
 
 
-    // Testimonial carousel
-
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: true,
-        dots: true,
-        loop: true,
-        margin: 0,
-        nav : true,
-        navText: false,
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
      // Fact Counter
 
      $(document).ready(function(){
@@ -152,8 +123,5 @@
             }
         });
     });
-
-
-
 })(jQuery);
 
